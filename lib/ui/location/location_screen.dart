7cb20c87f5299ca4d0e39507cfb2a605944f94d5
Amnah Ali <amnah1li@gmail.com>
2022-03-21 22:04:09 +0300

    remove background from location screen

--- lib/ui/location/location_screen.dart
+++ lib/ui/location/location_screen.dart
@@ -13,72 +13,69 @@ class LocationScreen extends GetWidget<LocationController> {
     return GetBuilder<LocationController>(
       init: LocationController(),
       builder: (controller) => Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(image: kDecorationImage),
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      MaterialButton(
-                        onPressed: () async {
-                          var weatherData =
-                              await WeatherModel().getLocationWeather();
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    MaterialButton(
+                      onPressed: () async {
+                        var weatherData =
+                            await WeatherModel().getLocationWeather();
+                        controller.getData(weatherData);
+                      },
+                      child: const Icon(
+                        Icons.refresh,
+                        size: 40.0,
+                      ),
+                    ),
+                    MaterialButton(
+                      onPressed: () async {
+                        var typeName = await Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const SearchScreen();
+                        }));
+                        if (typeName != null) {
+                          var weatherData = await WeatherModel()
+                              .getWeatherByCityName(typeName);
                           controller.getData(weatherData);
-                        },
-                        child: const Icon(
-                          Icons.refresh,
-                          size: 40.0,
-                        ),
+                        }
+                      },
+                      child: const Icon(
+                        Icons.location_city,
+                        size: 40.0,
                       ),
-                      MaterialButton(
-                        onPressed: () async {
-                          var typeName = await Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return const SearchScreen();
-                          }));
-                          if (typeName != null) {
-                            var weatherData = await WeatherModel()
-                                .getWeatherByCityName(typeName);
-                            controller.getData(weatherData);
-                          }
-                        },
-                        child: const Icon(
-                          Icons.location_city,
-                          size: 40.0,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                kSizeBoxHeight,
-                Center(
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        '${controller.weatherIcon}',
-                        style: kConditionTextStyle,
-                      ),
-                      Text(
-                        '${controller.temperature}°',
-                        style: kTempTextStyle,
-                      ),
-                    ],
-                  ),
+              ),
+              kSizeBoxHeight,
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '${controller.weatherIcon}',
+                      style: kConditionTextStyle,
+                    ),
+                    Text(
+                      '${controller.temperature}°',
+                      style: kTempTextStyle,
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 16.0, left: 16.0),
-                  child: Text(
-                    '${controller.weatherMessage} in \n${controller.cityName}',
-                    textAlign: TextAlign.center,
-                    style: kMessageTextStyle,
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0, left: 16.0),
+                child: Text(
+                  '${controller.weatherMessage} in \n${controller.cityName}',
+                  textAlign: TextAlign.center,
+                  style: kMessageTextStyle,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
