c01a30b5043060bb14645201196311cb1265ad9a
Amnah Ali <amnah1li@gmail.com>
2022-03-22 18:08:59 +0300

    add responses of weather

--- lib/ui/search/search_controller.dart
+++ lib/ui/search/search_controller.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
-import 'package:weatherflutter/data/location_data.dart';
-
+import '../../model/data/location_data.dart';
 import '../../network/weather_model.dart';
 
 class SearchController extends GetxController {
